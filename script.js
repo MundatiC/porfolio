87f5108949d1859d2aa3f02c2593637695995bd0
MundatiC <calebngethe230@gmail.com>
2024-04-23 14:04:16 +0300

    adds photo essay

--- script.js
+++ script.js
@@ -27,3 +27,50 @@ function downloadPDF(language) {
   }
   window.open(pdfFile);
 }
+
+const carouselInner = document.querySelector('.carousel-inner');
+const carouselItems = carouselInner.querySelectorAll('.carousel-item');
+const prevButton = document.querySelector('.carousel-control-prev');
+const nextButton = document.querySelector('.carousel-control-next');
+
+let currentIndex = 0;
+let intervalId;
+
+// Function to show the current carousel item
+function showItem(index) {
+  carouselItems.forEach((item, i) => {
+    if (i === index) {
+      item.classList.add('active');
+    } else {
+      item.classList.remove('active');
+    }
+  });
+}
+
+// Function to move to the next carousel item
+function nextItem() {
+  currentIndex = (currentIndex + 1) % carouselItems.length;
+  showItem(currentIndex);
+}
+
+// Function to move to the previous carousel item
+function prevItem() {
+  currentIndex = (currentIndex - 1 + carouselItems.length) % carouselItems.length;
+  showItem(currentIndex);
+}
+
+
+
+// Stop the carousel
+function stopCarousel() {
+  clearInterval(intervalId);
+}
+
+// Event listeners for navigation controls
+prevButton.addEventListener('click', prevItem);
+nextButton.addEventListener('click', nextItem);
+
+
+
+// Show the first item and start the carousel
+showItem(currentIndex);
